81de3c90fafa4194806606782059bf42c0f225d4
ram-mashruwala <113854904+ram-mashruwala@users.noreply.github.com>
2025-10-26 21:09:58 -0700

    Add title to App

--- client-side-new/src/App.svelte
+++ client-side-new/src/App.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
 </script>
 
+<h1>Restaurant</h1>
 
 <style>
+    h1 {
+        text-align: center;
+        font-size: 3.5rem;
+        font-family: Arial, Helvetica, sans-serif;
+    }
 </style>
