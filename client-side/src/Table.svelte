3c5021ac05b9fbce6ded74eebb33660013e45fcc
ram-mashruwala <113854904+ram-mashruwala@users.noreply.github.com>
2025-10-26 20:42:27 -0700

    Changed temp tables from text to button on top of brown circle

--- client-side/src/Table.svelte
+++ client-side/src/Table.svelte
@@ -1,1 +1,20 @@
-<p>Temp Table</p>
+<div>
+	<button>Order</button>
+</div>
+
+<style>
+	div {
+		width: 10em;
+		height: 10em;
+		border-radius: 50%;
+		background-color: rgb(110, 57, 0);
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	button:hover {
+		cursor: pointer;
+	}
+</style>
