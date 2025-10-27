7390058811864decd8ed3c10e479795657dd0049
ram-mashruwala <113854904+ram-mashruwala@users.noreply.github.com>
2025-10-26 21:20:12 -0700

    Added 4 Tables in App component

--- client-side-new/src/App.svelte
+++ client-side-new/src/App.svelte
@@ -1,12 +1,34 @@
 <script lang="ts">
+    import Table from "./Table.svelte";
 </script>
 
 <h1>Restaurant</h1>
+
+<div>
+    <Table />
+    <Table />
+    <div class="flex-break"></div>
+    <Table />
+    <Table />
+</div>
 
 <style>
     h1 {
         text-align: center;
         font-size: 3.5rem;
         font-family: Arial, Helvetica, sans-serif;
+    }
+
+    div {
+        display: flex;
+        justify-content: space-around;
+        flex-wrap: wrap;
+        row-gap: 100px;
+        margin-top: 200px;
+    }
+
+    .flex-break {
+        flex-basis: 100%;
+        height: 0px;
     }
 </style>
