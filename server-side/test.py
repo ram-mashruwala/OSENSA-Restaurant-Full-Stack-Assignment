420fa61e5eb76d2d20a844b54a2d227bbf854492
ram-mashruwala <113854904+ram-mashruwala@users.noreply.github.com>
2025-10-28 11:25:54 -0700

    Added tests for on_disconnect callback function

--- server-side/test.py
+++ server-side/test.py
@@ -2,7 +2,7 @@ import unittest
 from unittest.mock import Mock
 
 # import the callback from your module
-from main import on_connect
+from main import on_connect, on_disconnect
 
 
 class TestOnConnect(unittest.TestCase):
@@ -29,6 +29,19 @@ class TestOnConnect(unittest.TestCase):
         connected = on_connect(client, userdata, flags, rc, properties)
 
         self.assertFalse(connected)
+
+
+class TestOnDisconncet(unittest.TestCase):
+    def test_disconnect(self):
+        client = Mock()
+        userdata = None
+        flags = {}
+        rc = 0
+        properties = None
+
+        running = on_disconnect(client, userdata, flags, rc, properties)
+
+        self.assertFalse(running)
 
 
 if __name__ == "__main__":
